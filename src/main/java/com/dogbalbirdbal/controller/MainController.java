70554ee54b4a89454f1c2afa4eb3adeb77f2856d
sangjun <sangjun1389@ajou.ac.kr>
2024-01-01 23:53:25 +0900

    refactor: Move business logic to service layer

--- src/main/java/com/dogbalbirdbal/controller/MainController.java
+++ src/main/java/com/dogbalbirdbal/controller/MainController.java
@@ -1,23 +1,11 @@
 package com.dogbalbirdbal.controller;
 
-import com.dogbalbirdbal.database.manager.DataBaseServiceManager;
 import com.dogbalbirdbal.database.vo.PlaceInfo;
 import com.dogbalbirdbal.database.vo.UserInfo;
-import com.dogbalbirdbal.database.vo.WishBox;
-import com.dogbalbirdbal.database.vo.WishContainer;
-import com.dogbalbirdbal.database.vo.WishList;
 import com.dogbalbirdbal.service.CrawlingService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import com.dogbalbirdbal.service.UserService;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import lombok.RequiredArgsConstructor;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,122 +18,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
 
     private final CrawlingService crawlingService;
-    private final DataBaseServiceManager dataBaseServiceManager;
+    private final UserService userService;
 
-    @PostMapping("api/login/")
+    @PostMapping("/api/login")
     public HashMap<String, String> login(@RequestBody UserInfo userInfo) {
-        HashMap<String, String> stringStringHashMap = new HashMap<>();
-        boolean existData = false;
-
-        try (Connection con = dataBaseServiceManager.getConnection()) {
-            String sql = "select uid, name from myuser where uid = ? and password = ?";
-            PreparedStatement p = con.prepareStatement(sql);
-            p.setString(1, userInfo.getId());
-            p.setString(2, userInfo.getPassword());
-            ResultSet resultSet = p.executeQuery();
-
-            while (resultSet.next()) {
-                stringStringHashMap.put("id", userInfo.getId());
-                stringStringHashMap.put("name", resultSet.getString(2));
-                stringStringHashMap.put("token", userInfo.getId());
-                System.out.println(userInfo.getId() + " 로그인 성공");
-                return stringStringHashMap;
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-
-        stringStringHashMap.put("id", "fail");
-        stringStringHashMap.put("token", "-1");
-        return stringStringHashMap;
+        return userService.login(userInfo);
     }
 
-    @PostMapping("api/signup/")
+    @PostMapping("/api/signup")
     public String signup(@RequestBody UserInfo userInfo) {
-
-        try (Connection connect = dataBaseServiceManager.getConnection()) {
-            String sql = "insert into MyUser(uid, name, password, email) values(?, ?, ?, ?)";
-            PreparedStatement p = connect.prepareStatement(sql);
-            p.setString(1, userInfo.getId());
-            p.setString(2, userInfo.getName());
-            p.setString(3, userInfo.getPassword());
-            p.setString(4, userInfo.getEmail());
-            p.executeUpdate();
-            System.out.println(
-                    "ID: " + userInfo.getId() + " NAME: " + userInfo.getName() + " PW: " + userInfo.getPassword()
-                            + " EMAIL: " + userInfo.getEmail());
-            return "id : " + userInfo.getId() + ", name : " + userInfo.getName() + ", email : " + userInfo.getEmail()
-                    + ", password " + userInfo.getPassword();
-
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-            System.out.println("ID 중복");
-        }
-        return "Duplicate";
+        return userService.signUp(userInfo);
     }
 
-    @GetMapping("api/myinfo/{id}")
-    public HashMap<String, String> myInfoControllers(@PathVariable String id) throws SQLException {
-        LinkedHashMap<String, String> stringStringLinkedHashMap = new LinkedHashMap<>();
+    @GetMapping("/api/myinfo/{id}")
+    public HashMap<String, String> myInfo(@PathVariable String id){
+        return userService.myInfo(id);
+    }
 
-        try (Connection connect = dataBaseServiceManager.getConnection()) {
-            String sql1 = "select uid, name, email\n" +
-                    "from MyUser\n" +
-                    "where uid = ? ";
-            PreparedStatement p1 = connect.prepareStatement(sql1);
-            p1.setString(1, id);
-            ResultSet resultSet1 = p1.executeQuery();
-
-            while (resultSet1.next()) {
-                System.out.println(resultSet1.getString(1) + " mypage");
-                stringStringLinkedHashMap.put("id", resultSet1.getString(1));
-                stringStringLinkedHashMap.put("name", resultSet1.getString(2));
-                stringStringLinkedHashMap.put("email", resultSet1.getString(3));
-            }
-            String sql2 = "select string_to_array(route, ',') from wishlist";
-            PreparedStatement p2 = connect.prepareStatement(sql2);
-            ResultSet resultSet2 = p2.executeQuery();
-
-            try (Connection connection2 = dataBaseServiceManager.getConnection()) {
-                String sql3 = "select route from wishlist where uid = ?";
-                PreparedStatement p3 = connection2.prepareStatement(sql3);
-                p3.setString(1, id);
-                ResultSet resultSet = p3.executeQuery();
-                WishContainer wishContainer = new WishContainer();
-
-                while (resultSet.next()) {
-                    WishBox wishBox = new WishBox();
-                    String route = resultSet.getString(1);
-                    JSONParser jsonParser = new JSONParser();
-
-                    try {
-                        JSONArray jsonArray = (JSONArray) jsonParser.parse(route);
-                        jsonArray.forEach(o -> {
-                            JSONObject jsonObject = (JSONObject) o;
-                            String name = jsonObject.get("name").toString();
-                            String picURL = jsonObject.get("pic_url").toString();
-                            String info = jsonObject.get("info").toString();
-                            wishBox.addWishList(new WishList(name, picURL, info));
-                        });
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    wishContainer.addWishBox(wishBox);
-                }
-
-                try {
-                    ObjectMapper objectMapper = new ObjectMapper();
-                    String jsonData = objectMapper.writeValueAsString(wishContainer);
-                    stringStringLinkedHashMap.put("result", jsonData);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return stringStringLinkedHashMap;
+    @PostMapping("/api/myinfo/wishlist")
+    public String addRoute(@RequestBody PlaceInfo placeInfo) {
+        return userService.addRoute(placeInfo);
     }
 
     @GetMapping("/api/crawlingfood/{location}")
@@ -168,21 +60,5 @@ public class MainController {
         String theme = pathInfo.split(" ")[1];
 
         return crawlingService.selectPath(destination, theme);
-    }
-
-    @PostMapping("api/myinfo/wishlist/")
-    public String routesender(@RequestBody PlaceInfo placeInfo) {
-
-        try (Connection connect = dataBaseServiceManager.getConnection()) {
-            String sql = "insert into wishlist(uid, route) values(?, ?)";
-            PreparedStatement p = connect.prepareStatement(sql);
-            p.setString(1, placeInfo.getId());
-            p.setString(2, placeInfo.getRoute());
-            p.executeUpdate();
-
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-        return "id : " + placeInfo.getId() + ", route " + placeInfo.getRoute();
     }
 }
---
+++ src/main/java/com/dogbalbirdbal/service/UserService.java
@@ -0,0 +1,152 @@
+package com.dogbalbirdbal.service;
+
+import com.dogbalbirdbal.database.manager.DataBaseServiceManager;
+import com.dogbalbirdbal.database.vo.PlaceInfo;
+import com.dogbalbirdbal.database.vo.UserInfo;
+import com.dogbalbirdbal.database.vo.WishBox;
+import com.dogbalbirdbal.database.vo.WishContainer;
+import com.dogbalbirdbal.database.vo.WishList;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final DataBaseServiceManager dataBaseServiceManager;
+
+    public HashMap<String, String> login(UserInfo userInfo){
+        HashMap<String, String> stringStringHashMap = new HashMap<>();
+
+        try (Connection con = dataBaseServiceManager.getConnection()) {
+            String sql = "select uid, name from myuser where uid = ? and password = ?";
+            PreparedStatement p = con.prepareStatement(sql);
+            p.setString(1, userInfo.getId());
+            p.setString(2, userInfo.getPassword());
+            ResultSet resultSet = p.executeQuery();
+
+            if (resultSet.next()) {
+                stringStringHashMap.put("id", userInfo.getId());
+                stringStringHashMap.put("name", resultSet.getString(2));
+                stringStringHashMap.put("token", userInfo.getId());
+                System.out.println(userInfo.getId() + " 로그인 성공");
+                return stringStringHashMap;
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        stringStringHashMap.put("id", "fail");
+        stringStringHashMap.put("token", "-1");
+        return stringStringHashMap;
+    }
+
+    public String signUp(UserInfo userInfo) {
+        try (Connection connect = dataBaseServiceManager.getConnection()) {
+            String sql = "insert into MyUser(uid, name, password, email) values(?, ?, ?, ?)";
+            PreparedStatement p = connect.prepareStatement(sql);
+            p.setString(1, userInfo.getId());
+            p.setString(2, userInfo.getName());
+            p.setString(3, userInfo.getPassword());
+            p.setString(4, userInfo.getEmail());
+            p.executeUpdate();
+            System.out.println(
+                    "ID: " + userInfo.getId() + " NAME: " + userInfo.getName() + " PW: " + userInfo.getPassword()
+                            + " EMAIL: " + userInfo.getEmail());
+            return "id : " + userInfo.getId() + ", name : " + userInfo.getName() + ", email : " + userInfo.getEmail()
+                    + ", password " + userInfo.getPassword();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            System.out.println("ID 중복");
+        }
+        return "Duplicate";
+    }
+
+    public HashMap<String, String> myInfo(String id) {
+        LinkedHashMap<String, String> stringStringLinkedHashMap = new LinkedHashMap<>();
+
+        try (Connection connect = dataBaseServiceManager.getConnection()) {
+            String sql1 = "select uid, name, email\n" +
+                    "from MyUser\n" +
+                    "where uid = ? ";
+            PreparedStatement p1 = connect.prepareStatement(sql1);
+            p1.setString(1, id);
+            ResultSet resultSet1 = p1.executeQuery();
+
+            while (resultSet1.next()) {
+                System.out.println(resultSet1.getString(1) + " mypage");
+                stringStringLinkedHashMap.put("id", resultSet1.getString(1));
+                stringStringLinkedHashMap.put("name", resultSet1.getString(2));
+                stringStringLinkedHashMap.put("email", resultSet1.getString(3));
+            }
+            String sql2 = "select string_to_array(route, ',') from wishlist";
+            PreparedStatement p2 = connect.prepareStatement(sql2);
+            ResultSet resultSet2 = p2.executeQuery();
+
+            try (Connection connection2 = dataBaseServiceManager.getConnection()) {
+                String sql3 = "select route from wishlist where uid = ?";
+                PreparedStatement p3 = connection2.prepareStatement(sql3);
+                p3.setString(1, id);
+                ResultSet resultSet = p3.executeQuery();
+                WishContainer wishContainer = new WishContainer();
+
+                while (resultSet.next()) {
+                    WishBox wishBox = new WishBox();
+                    String route = resultSet.getString(1);
+                    JSONParser jsonParser = new JSONParser();
+
+                    try {
+                        JSONArray jsonArray = (JSONArray) jsonParser.parse(route);
+                        jsonArray.forEach(o -> {
+                            JSONObject jsonObject = (JSONObject) o;
+                            String name = jsonObject.get("name").toString();
+                            String picURL = jsonObject.get("pic_url").toString();
+                            String info = jsonObject.get("info").toString();
+                            wishBox.addWishList(new WishList(name, picURL, info));
+                        });
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    wishContainer.addWishBox(wishBox);
+                }
+
+                try {
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    String jsonData = objectMapper.writeValueAsString(wishContainer);
+                    stringStringLinkedHashMap.put("result", jsonData);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return stringStringLinkedHashMap;
+    }
+
+    public String addRoute(PlaceInfo placeInfo) {
+
+        try (Connection connect = dataBaseServiceManager.getConnection()) {
+            String sql = "insert into wishlist(uid, route) values(?, ?)";
+            PreparedStatement p = connect.prepareStatement(sql);
+            p.setString(1, placeInfo.getId());
+            p.setString(2, placeInfo.getRoute());
+            p.executeUpdate();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return "id : " + placeInfo.getId() + ", route " + placeInfo.getRoute();
+    }
+}
